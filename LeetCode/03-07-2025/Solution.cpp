3f753a79504bc7eaeb37868e9b0bf4d1fab1aab1
unknown <guptadivyansh357@gmail.com>
2025-07-03 22:14:26 +0530

    03-07-2025 LeetCode

---
+++ LeetCode/03-07-2025/Solution.cpp
@@ -0,0 +1,39 @@
+// Problem
+/*
+Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+
+You are given a positive integer k.
+
+Now Bob will ask Alice to perform the following operation forever:
+
+Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+
+Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+
+Note that the character 'z' can be changed to 'a' in the operation.
+*/
+
+// Code
+
+class Solution {
+public:
+    char kthCharacter(int k) {
+        
+        string res = "a";
+
+        while(res.size() < k) {
+
+            string temp = "";
+            for(auto it: res) {
+
+                if(it == 'z')   temp += 'a';
+                else    temp += it + 1;
+            }
+
+            res += temp;
+        }
+
+        return res[k - 1];
+    }
+};
