cb4bc25b2e842f5a2ca180d428e9b148c18694c0
unknown <guptadivyansh357@gmail.com>
2025-07-14 17:58:14 +0530

    14-07-2025 LeetCode

---
+++ LeetCode/14-07-2025/Solution.cpp
@@ -0,0 +1,27 @@
+// Problem
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+*/
+
+// Code
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        
+        string s;
+        ListNode* temp = head;
+
+        while(temp) {
+
+            s = s + (char)(temp->val + '0');
+            temp = temp->next;
+        }
+
+        return stoi(s, nullptr, 2);
+    }
+};
